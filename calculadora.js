f35d3cb7139abf52055d07d496828357797981ee
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-08-25 21:59:31 -0300

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+/* function insert(num) {
+  var numero = document.getElementById("soma").innerHTML;
+  document.getElementById("soma").innerHTML = numero + num;
+}
+function clean() {
+  document.getElementById("soma").innerHTML = "";
+}
+function back() {
+  var soma = document.getElementById("soma").innerHTML;
+  document.getElementById("soma").innerHTML = soma.substring(
+    0,
+    soma.length - 1
+  );
+}
+function calcular() {
+  var resultado = document.getElementById("soma").innerHTML;
+  if (resultado) {
+    document.getElementById("resultado").innerHTML = eval(resultado);
+  } else {
+    document.getElementById("resultado").innerHTML = "Nada...";
+  }
+}
+ */
